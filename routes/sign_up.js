3f7732f34011347affa0f71cfae773b35596d242
Aqil <k181054@nu.edu.pk>
2021-08-17 16:49:11 +0500

    make it better

--- routes/sign_up.js
+++ routes/sign_up.js
@@ -1,3 +1,4 @@
+const { query } = require('../database/db');
 const db = require('../database/db')
 
 const router=require('express').Router();
@@ -14,12 +15,26 @@ router.post('/sign_up',async(req,res)=>{
     mobile=mobile ? mobile:null
     country=country ? country:null
     console.log(name,password,role_id)
-    try {
-        await db.query('Insert into users(name,password,email,role_id,NIC ,mobile,city, country) values($1,$2,$3,$4,$5,$6,$7,$8)',[name,password,email,role_id,NIC ,mobile,city, country])
-        res.status(200).send('Signup Successfull')
-    } catch (error) {
-        console.log(error)
+    if(email && password)
+    {
+        try {
+            user=await db.query('Select user_id from users where email=$1',[email])
+            console.log(user.rows[0])
+        } catch (error) {
+         console.log(error)   
+        }
+        try {
+            await db.query('Insert into users(name,password,email,role_id,NIC ,mobile,city, country) values($1,$2,$3,$4,$5,$6,$7,$8)',[name,password,email,role_id,NIC ,mobile,city, country])
+            res.status(200).send('Signup Successfull')
+        } catch (error) {
+            console.log(error)
+        }
     }
+    else
+    {
+        res.status(401).send({error:"Missing Credentials"})
+    }
+
 })
 
 
